dd9c32e160d3d65f0e1c7059850770760e4d3d60
FameZeeshan <zeeshan6143@gmail.com>
2023-07-21 10:35:53 +0530

    json reader fixed with correct boundingBox values

--- src/App.js
+++ src/App.js
@@ -1,25 +1,107 @@
-import logo from './logo.svg';
-import './App.css';
+import React, { useState } from "react";
+import pdfData from "./sample.json";
+import pdfFile from "./sample.pdf";
 
-function App() {
+const PdfHighlighterApp = () => {
+  const [highlightedBoundingBox, setHighlightedBoundingBox] = useState(null);
+
+  // Function to handle the button click and search for the bounding box
+  const handleButtonClick = (searchText) => {
+    let boundingBox = null;
+    const regex = new RegExp(`\\b${searchText}\\b`, "i");
+
+    for (const readResult of pdfData.analyzeResult.readResults) {
+      for (const line of readResult.lines) {
+        if (regex.test(line.text)) {
+          boundingBox = line.boundingBox;
+          const word = line.words.find((word) => regex.test(word.text));
+          if (word && word.boundingBox.length === 8) {
+            boundingBox = word.boundingBox;
+            break;
+          }
+        }
+      }
+      if (boundingBox) break;
+    }
+
+    console.log("Search Text:", searchText);
+    console.log("Bounding Box:", boundingBox);
+
+    setHighlightedBoundingBox(boundingBox);
+  };
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+      <h1>PDF Highlighter App</h1>
+      <button onClick={() => handleButtonClick("UNITED STATES")}>
+        Search for "UNITED STATES"
+      </button>
+      <button onClick={() => handleButtonClick("UNITED")}>
+        Search for "UNITED"
+      </button>
+
+      <div className="pdf-container">
+        <iframe
+          src={pdfFile} // Replace with the actual path to your PDF file
+          title="PDF Viewer"
+          width="100%"
+          height="600px"
+          frameBorder="0"
+        />
+        {highlightedBoundingBox && (
+          <div
+            className="highlight-box"
+            style={{
+              left: `${
+                Math.min(
+                  highlightedBoundingBox[0],
+                  highlightedBoundingBox[2],
+                  highlightedBoundingBox[4],
+                  highlightedBoundingBox[6]
+                ) * 100
+              }%`,
+              top: `${
+                Math.min(
+                  highlightedBoundingBox[1],
+                  highlightedBoundingBox[3],
+                  highlightedBoundingBox[5],
+                  highlightedBoundingBox[7]
+                ) * 100
+              }%`,
+              width: `${
+                Math.max(
+                  highlightedBoundingBox[0],
+                  highlightedBoundingBox[2],
+                  highlightedBoundingBox[4],
+                  highlightedBoundingBox[6]
+                ) -
+                Math.min(
+                  highlightedBoundingBox[0],
+                  highlightedBoundingBox[2],
+                  highlightedBoundingBox[4],
+                  highlightedBoundingBox[6]
+                )
+              }%`,
+              height: `${
+                Math.max(
+                  highlightedBoundingBox[1],
+                  highlightedBoundingBox[3],
+                  highlightedBoundingBox[5],
+                  highlightedBoundingBox[7]
+                ) -
+                Math.min(
+                  highlightedBoundingBox[1],
+                  highlightedBoundingBox[3],
+                  highlightedBoundingBox[5],
+                  highlightedBoundingBox[7]
+                )
+              }%`,
+            }}
+          />
+        )}
+      </div>
     </div>
   );
-}
+};
 
-export default App;
+export default PdfHighlighterApp;
