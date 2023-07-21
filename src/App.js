36c10a97280c5f849d258cb91856d54776c0201d
FameZeeshan <zeeshan6143@gmail.com>
2023-07-21 11:30:45 +0530

    highlighter is showing but the position is not fixed

--- src/App.js
+++ src/App.js
@@ -1,6 +1,8 @@
 import React, { useState } from "react";
+import { Document, Page } from "react-pdf/dist/esm/entry.webpack";
 import pdfData from "./sample.json";
 import pdfFile from "./sample.pdf";
+import "./pdfStyles.css"; // Import the CSS file
 
 const PdfHighlighterApp = () => {
   const [highlightedBoundingBox, setHighlightedBoundingBox] = useState(null);
@@ -8,20 +10,17 @@ const PdfHighlighterApp = () => {
   // Function to handle the button click and search for the bounding box
   const handleButtonClick = (searchText) => {
     let boundingBox = null;
-    const regex = new RegExp(`\\b${searchText}\\b`, "i");
 
-    for (const readResult of pdfData.analyzeResult.readResults) {
-      for (const line of readResult.lines) {
-        if (regex.test(line.text)) {
-          boundingBox = line.boundingBox;
-          const word = line.words.find((word) => regex.test(word.text));
-          if (word && word.boundingBox.length === 8) {
-            boundingBox = word.boundingBox;
-            break;
-          }
-        }
+    for (let i = 0; i < pdfData.analyzeResult.readResults.length; i++) {
+      const line = pdfData.analyzeResult.readResults[i].lines.find(
+        (line) => line.text === searchText
+      );
+
+      // Check if the line with the search text is found
+      if (line && line.boundingBox.length === 8) {
+        boundingBox = line.boundingBox;
+        break;
       }
-      if (boundingBox) break;
     }
 
     console.log("Search Text:", searchText);
@@ -30,75 +29,72 @@ const PdfHighlighterApp = () => {
     setHighlightedBoundingBox(boundingBox);
   };
 
-  return (
-    <div>
-      <h1>PDF Highlighter App</h1>
-      <button onClick={() => handleButtonClick("UNITED STATES")}>
-        Search for "UNITED STATES"
-      </button>
-      <button onClick={() => handleButtonClick("UNITED")}>
-        Search for "UNITED"
-      </button>
+  // Function to calculate the highlighter position
+  const calculateHighlighterPosition = () => {
+    if (!highlightedBoundingBox) return {};
 
-      <div className="pdf-container">
-        <iframe
-          src={pdfFile} // Replace with the actual path to your PDF file
-          title="PDF Viewer"
-          width="100%"
-          height="600px"
-          frameBorder="0"
-        />
-        {highlightedBoundingBox && (
-          <div
-            className="highlight-box"
-            style={{
-              left: `${
-                Math.min(
-                  highlightedBoundingBox[0],
-                  highlightedBoundingBox[2],
-                  highlightedBoundingBox[4],
-                  highlightedBoundingBox[6]
-                ) * 100
-              }%`,
-              top: `${
-                Math.min(
-                  highlightedBoundingBox[1],
-                  highlightedBoundingBox[3],
-                  highlightedBoundingBox[5],
-                  highlightedBoundingBox[7]
-                ) * 100
-              }%`,
-              width: `${
-                Math.max(
-                  highlightedBoundingBox[0],
-                  highlightedBoundingBox[2],
-                  highlightedBoundingBox[4],
-                  highlightedBoundingBox[6]
-                ) -
-                Math.min(
-                  highlightedBoundingBox[0],
-                  highlightedBoundingBox[2],
-                  highlightedBoundingBox[4],
-                  highlightedBoundingBox[6]
-                )
-              }%`,
-              height: `${
-                Math.max(
-                  highlightedBoundingBox[1],
-                  highlightedBoundingBox[3],
-                  highlightedBoundingBox[5],
-                  highlightedBoundingBox[7]
-                ) -
-                Math.min(
-                  highlightedBoundingBox[1],
-                  highlightedBoundingBox[3],
-                  highlightedBoundingBox[5],
-                  highlightedBoundingBox[7]
-                )
-              }%`,
-            }}
-          />
-        )}
+    const pdfDPI = 72; // Assuming the DPI of the PDF is 72
+    const pdfWidthInInches = 8.5; // Width of the PDF in inches
+    const pdfHeightInInches = 11; // Height of the PDF in inches
+
+    // Convert the bounding box coordinates from inches to pixels
+    const pdfWidthInPixels = pdfWidthInInches * pdfDPI;
+    const pdfHeightInPixels = pdfHeightInInches * pdfDPI;
+
+    return {
+      left: `${(highlightedBoundingBox[0] / pdfWidthInPixels) * 100}%`,
+      top: `${(highlightedBoundingBox[1] / pdfHeightInPixels) * 100}%`,
+      width: `${
+        ((highlightedBoundingBox[2] - highlightedBoundingBox[0]) /
+          pdfWidthInPixels) *
+        100
+      }%`,
+      height: `${
+        ((highlightedBoundingBox[3] - highlightedBoundingBox[1]) /
+          pdfHeightInPixels) *
+        100
+      }%`,
+    };
+  };
+
+  return (
+    <div style={{ display: "flex" }}>
+      {/* Left side with PDF */}
+      <div style={{ flex: 1 }}>
+        <h5>PDF Highlighter App</h5>
+        <div className="pdf-container">
+          <Document file={pdfFile}>
+            {/* Render each page with the "pdf-page" class */}
+            <Page pageNumber={1} className="pdf-page" />
+          </Document>
+          {highlightedBoundingBox && (
+            <div
+              className="highlight-box"
+              style={{
+                position: "absolute",
+                border: "3px solid red", // You can adjust the border style as needed
+                ...calculateHighlighterPosition(),
+              }}
+            />
+          )}
+        </div>
+      </div>
+
+      {/* Right side with content */}
+      <div
+        style={{
+          flex: 1,
+          display: "flex",
+          flexDirection: "column",
+          alignItems: "center",
+        }}
+      >
+        {/* Add your content here */}
+        <h5>Form</h5>
+
+        <button onClick={() => handleButtonClick("UNITED STATES")}>
+          Search for "UNITED STATES"
+        </button>
       </div>
     </div>
   );
