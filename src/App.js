d5d1c90fa81a26ab2a6c1f2f00459462c27fb9cf
FameZeeshan <zeeshan6143@gmail.com>
2023-08-10 07:35:01 +0530

    text content display none to disable it

--- src/App.js
+++ src/App.js
@@ -1,44 +1,38 @@
 import React, { useState } from "react";
-import { Document, Page } from "react-pdf/dist/esm/entry.webpack"; // Import necessary components from the react-pdf library
-import pdfData from "./sample.json"; // Import the JSON file containing extracted text and bounding box data
-import pdfFile from "./sample.pdf"; // Import the PDF file to be displayed
+import { Document, Page, pdfjs } from "react-pdf"; // Import necessary components from the react-pdf library
+import pdfData from "./sample.json";
+import pdfFile from "./sample.pdf";
 import "./pdfStyles.css"; // Import the CSS file for styling
 
+// Set the worker URL for pdfjs
+pdfjs.GlobalWorkerOptions.workerSrc = `//cdnjs.cloudflare.com/ajax/libs/pdf.js/${pdfjs.version}/pdf.worker.js`;
+
 const PdfHighlighterApp = () => {
-  // State to store the currently highlighted bounding box
   const [highlightedBoundingBox, setHighlightedBoundingBox] = useState(null);
 
-  // Function to handle the button click and search for the bounding box
   const handleButtonClick = (searchText) => {
-    // Search for the line containing the searchText in the extracted text data (pdfData)
     const line = pdfData.analyzeResult.readResults[0].lines.find((line) =>
       line.text.includes(searchText)
     );
 
-    // Check if the line with the search text is found
     if (line && line.boundingBox.length === 8) {
-      // If found, set the bounding box of the line as the currently highlighted bounding box
       const boundingBox = line.boundingBox;
       console.log("Search Text:", searchText);
       console.log("Bounding Box:", boundingBox);
       setHighlightedBoundingBox(boundingBox);
     } else {
-      // If not found, clear the currently highlighted bounding box
       console.log("Search Text:", searchText);
       console.log("No matching bounding box found.");
       setHighlightedBoundingBox(null);
     }
   };
 
-  // Function to calculate the highlighter position
   const calculateHighlighterPosition = () => {
     if (!highlightedBoundingBox) return {};
 
-    // Assuming the bounding box coordinates are in inches
-    const pdfWidth = 8.5; // Width of the PDF in inches
-    const pdfHeight = 11; // Height of the PDF in inches
+    const pdfWidth = 8.5;
+    const pdfHeight = 11;
 
-    // Calculate and return the position of the highlighter box as a percentage of the PDF dimensions
     return {
       left: `${(highlightedBoundingBox[0] / pdfWidth) * 100}%`,
       top: `${(highlightedBoundingBox[1] / pdfHeight) * 100}%`,
@@ -72,18 +66,16 @@ const PdfHighlighterApp = () => {
           }}
           className="pdf-container"
         >
-          {/* Render the PDF document */}
+          {/* Render the PDF document using the Document and Page components */}
           <Document file={pdfFile}>
-            {/* Render the first page of the PDF */}
             <Page pageNumber={1} className="pdf-page" />
           </Document>
-          {/* Show the highlighted bounding box if available */}
           {highlightedBoundingBox && (
             <div
               className="highlight-box"
               style={{
                 position: "absolute",
-                border: "3px solid red", // You can adjust the border style as needed
+                border: "3px solid red",
                 ...calculateHighlighterPosition(),
               }}
             />
@@ -104,7 +96,7 @@ const PdfHighlighterApp = () => {
         {/* Add your content here */}
         <h5>Form</h5>
 
-        {/* Add buttons with different search texts to highlight specific content */}
+        {/* Add buttons with different search texts */}
         <div className="workingbtn">
           <button onClick={() => handleButtonClick("UNITED STATES")}>
             Search for "Country"
